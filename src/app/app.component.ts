00fa2cdfc5429c05cba3370054b035f1b1d52a8d
Daniel Schiavini <daniel.schiavini@gmail.com>
2020-01-25 21:19:40 +0100

    Simple line

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as d3 from 'd3';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'snapshots';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    // The data for our line
+    const lineData: [number, number][] = [
+      [1,  5],  [20, 20],
+      [40, 10], [60, 40],
+      [80, 5],  [100, 60]
+    ];
+
+    // This is the accessor function we talked about above
+    const lineFunction = d3.line()
+       .x(([x, ]) => x)
+       .y(([, y]) => y)
+       .curve(d3.curveLinear);
+
+    const svgContainer = d3.select('#graph-container')
+      .append('svg')
+      .attr('width', 800)
+      .attr('height', 600);
+
+    svgContainer.append('path')
+      .attr('d', lineFunction(lineData))
+      .attr('stroke', 'blue')
+      .attr('stroke-width', 2)
+      .attr('fill', 'none');
+  }
 }
