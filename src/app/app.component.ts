ea8e52b5f753ecd3ead03aeaae039b3f2d799370
Daniel Schiavini <daniel.schiavini@gmail.com>
2020-01-25 21:37:55 +0100

    Scaling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,27 +9,30 @@ import * as d3 from 'd3';
 export class AppComponent implements OnInit {
   ngOnInit(): void {
     // The data for our line
-    const lineData: [number, number][] = [
-      [1,  5],  [20, 20],
-      [40, 10], [60, 40],
-      [80, 5],  [100, 60]
-    ];
+      const lineData = [ { x: 1,   y: 5},  { x: 20,  y: 20},
+                         { x: 40,  y: 10}, { x: 60,  y: 40},
+                         { x: 80,  y: 5},  { x: 100, y: 60}];
 
-    // This is the accessor function we talked about above
-    const lineFunction = d3.line()
-       .x(([x, ]) => x)
-       .y(([, y]) => y)
-       .curve(d3.curveLinear);
+      const yScale = d3.scaleLinear()
+            .domain([1, 100])
+            .range([100, 600]);
 
-    const svgContainer = d3.select('#graph-container')
-      .append('svg')
-      .attr('width', 800)
-      .attr('height', 600);
+      const xScale = d3.scaleLinear()
+            .domain([1, 100])
+            .range([0, 700]);
 
-    svgContainer.append('path')
-      .attr('d', lineFunction(lineData))
-      .attr('stroke', 'blue')
-      .attr('stroke-width', 2)
-      .attr('fill', 'none');
+          // This is the accessor function we talked about above
+      const line = d3.line();
+      d3.select('#graph-container')
+            .append('svg')
+            .attr('width', 800)
+            .attr('height', 600)
+
+            .append('path')
+            .data(lineData)
+            .attr('d', line(lineData.map(({x, y}) => [xScale(x), yScale(y)])))
+            .attr('stroke', 'blue')
+            .attr('stroke-width', 2)
+            .attr('fill', 'none');
   }
 }
